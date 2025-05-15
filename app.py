2ee25ec0969a7f65301ee015502fe732a9df75c0
Harsshal-web <htambave@gmail.com>
2025-05-15 16:12:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 
 # Set page config
-
-
-
 st.set_page_config(page_title="Flipkart Laptop Search", layout="centered")
 
 # Load model
